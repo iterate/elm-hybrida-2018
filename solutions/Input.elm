aeeb5f316689c824b8f06cb30b323c7a8246e8a4
awesomund <osmund@iterate.no>
2018-02-20 22:58:59 +0100

    del Input.elm opp i enkle steg

--- solutions/Input.elm
+++ solutions/Input.elm
@@ -1,17 +1,12 @@
 module Input exposing (..)
 
-import Html exposing (Html, div, input, text)
-import Html.Attributes exposing (placeholder)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
 
-main : Program Never Model Msg
-main =
-    Html.beginnerProgram { model = model, view = view, update = update }
-
-
-
--- MODEL
+-- Oppgave 1: Model
+-- a) Lag en type alias som heter Model. Model er en Record som inneholder en string.
 
 
 type alias Model =
@@ -19,33 +14,58 @@ type alias Model =
     }
 
 
+
+-- b) Lag en record, model, som er en Model.
+
+
 model : Model
 model =
-    { content = "" }
+    { content = ""
+    }
 
 
 
--- UPDATE
+-- Oppgave 2: Update
+-- a) Lag en type som heter Msg, som bare kan være én ting: UpdateContent String
 
 
 type Msg
     = UpdateContent String
 
 
+
+-- b) Lag en funksjon som heter "update". "update" tar inn en Msg og en Model, og returnerer en ny, oppdatert Model.
+--    Den oppdaterte modellen skal inneholde stringen som kom inn via Msg.
+
+
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        UpdateContent newContent ->
-            { model | content = newContent }
+        UpdateContent string ->
+            { model | content = string }
 
 
 
--- VIEW
+-- Oppgave 3: View
+-- a) Lag en funksjon som heter "view". Den tar inn en Model og returnerer Html Msg.
+-- b) view skal vise stringen i model.
+-- c) view skal også inneholde et tekst-input-felt, som genererer en UpdateText når man skriver noe.
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ input [ placeholder "Text to show", onInput UpdateContent ] []
-        , div [] [ text ("Current content:  " ++ model.content) ]
+        [ input [ placeholder "skriv noe her", onInput UpdateContent ] []
+        , div [] [ text (model.content) ]
         ]
+
+
+
+-- Oppgave 4: Program
+-- Lag en funksjon, main, som returnerer et Html.beginnerProgram.
+-- beginnerProgram tar inn en record med model, view og update-funksjoner.
+
+
+main : Program Never Model Msg
+main =
+    beginnerProgram { model = model, update = update, view = view }
